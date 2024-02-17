6e8e6761e0f350f36cb7963bc62d6cadb2dc8ebd
fadlydestriana <fadlydestriana@gmail.com>
2024-02-17 22:00:15 +0700

    solving 15th-17th problem

--- pointer/exercise.c
+++ pointer/exercise.c
@@ -491,6 +491,139 @@ void answerNumber14(){
     free(numbers);
 }
 
+/*
+15. Write a C program to demonstrate how a function returns a pointer.
+    Test Data :
+    Input the first number : 5
+    Input the second number : 6
+    Expected Output :
+
+    The number 6 is larger.
+*/
+
+int* answerNumber15(){
+    int firstNumber, secondNumber;
+    int* ptr;
+    
+    printf("Input the first number : ");
+    scanf("%d", &firstNumber);
+
+    printf("Input the second number : ");
+    scanf("%d", &secondNumber);
+
+    if (firstNumber > secondNumber){
+        ptr = &firstNumber;
+        return ptr;
+    }else{
+        ptr = &secondNumber;
+        return ptr;
+    }
+    
+}
+
+/*
+16. Write a program in C to compute the sum of all elements in an array using pointers.
+    Test Data :
+    Input the number of elements to store in the array (max 10) : 5
+    Input 5 number of elements in the array :
+    element - 1 : 2
+    element - 2 : 3
+    element - 3 : 4
+    element - 4 : 5
+    element - 5 : 6
+    Expected Output :
+
+    The sum of array is : 20 
+*/
+
+void answerNumber16(){
+    int len, sum = 0;
+    int arr[10];
+    int* ptr;
+
+    printf("Input the number of elements to store in the array (max 10) : ");
+    scanf("%d", &len);
+    
+    if(len>10){
+        printf("The maximum number of elements exceeded\n");
+        exit(0);
+    }
+
+    printf("Input %d number of elements in the array : \n", len);
+
+    for(int i=0; i<len; i++){
+        printf("element - %d : ", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    ptr = arr;
+
+    for(int i=0; i<len; i++){
+        sum = sum + *ptr;
+        *ptr++;
+    }
+
+    printf("The sum of array is : %d\n", sum);
+}
+
+/*
+17. Write a program in C to print the elements of an array in reverse order.
+    Test Data :
+    Input the number of elements to store in the array (max 15) : 5
+    Input 5 number of elements in the array :
+    element - 1 : 2
+    element - 2 : 3
+    element - 3 : 4
+    element - 4 : 5
+    element - 5 : 6
+    Expected Output :
+
+    The elements of array in reverse order are :                                                                 
+    element - 5 : 6                                                                                              
+    element - 4 : 5                                                                                              
+    element - 3 : 4                                                                                              
+    element - 2 : 3                                                                                              
+    element - 1 : 2
+*/
+
+void answerNumber17(){
+    int n;
+    int arr[15];
+    int* ptr = &arr[0];
+
+    printf("Input the number of elements to store in the array (max 15) : ");
+    scanf("%d", &n);
+
+    if(n>15){
+        printf("The maximum number of elements is exceeded \n");
+        exit(0);
+    }
+
+    printf("Input %d number of elements in the array : \n", n);
+
+    for(int i=0; i<n; i++){
+        printf("element - %d : ", i+1);
+        scanf("%d", ptr);
+        ptr++;
+    }
+
+    int tmp;
+    for(int i=0; i<n; ++i){
+        for(int j=i+1; j<n; ++j){
+            if(arr[i]<arr[j]){
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+
+    printf("The elements of array in reverse order are : \n");
+    for(int i=0; i<n; i++){
+        printf("element - %d : %d\n", i+1, arr[i]);
+    }
+}
+
 // main function
 int main(){
     answerNumber1();
@@ -506,6 +639,9 @@ int main(){
     // answerNumber11();
     // answerNumber12();
     // answerNumber13();
-    answerNumber14();
+    // answerNumber14();
+    // printf("The number %d is larger.\n",*answerNumber15());
+    // answerNumber16();
+    answerNumber17();
     return 0;
 }
