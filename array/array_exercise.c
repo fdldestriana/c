c8ac85ad03e468d9fd87bab16eda712342af5ab7
fadlydestriana <fadlydestriana@gmail.com>
2024-02-29 22:06:45 +0700

    solving 16th and 17th problem from array exerise in C

--- array/array_exercise.c
+++ array/array_exercise.c
@@ -699,23 +699,90 @@ void answerNumber15(){
 */
 
 void answerNumber16(){
+    int len;
+    int max;
 
-    char string[] = "fadly";
-    
-    int len=0;
-    while (string != NULL && *string != '\0')
-    {
-        len++;
-        string+1;
+    printf("Input the of the array : ");
+    scanf("%d", &len);
+
+    printf("Input %d elements in the array : \n", len);
+
+    int arr[len];
+    for(int i=0; i<len; i++){
+        printf("element - %d : ", i);
+        scanf("%d", &arr[i]);
     }
-    
-    for (int i = 0; i < len; i++)
-    {
-        printf("%d", len);
+
+    max = arr[0];
+    for(int i=1; i<len; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
     }
-    
-    
+
+    int secondLargest = 0;
+    for(int i=0; i<len; i++){
+        if(arr[i] == max){
+            continue;
+        }else{
+            if(secondLargest < arr[i]){
+                secondLargest = arr[i];
+            }
+        }
+    }
+
+    printf("The Second smallest element in the array is : %d\n", secondLargest);
 }   
+
+/*
+17. Write a program in C to find the second smallest element in an array.
+    Test Data :
+    Input the size of array : 5
+    Input 5 elements in the array (value must be <9999) :
+    element - 0 : 0
+    element - 1 : 9
+    element - 2 : 4
+    element - 3 : 6
+    element - 4 : 5
+    Expected Output :
+    The Second smallest element in the array is : 4
+*/
+
+void answerNumber17(){
+    int len;
+    int min;
+
+    printf("Input the of the array : ");
+    scanf("%d", &len);
+
+    printf("Input %d elements in the array (value must be <9999) : \n", len);
+
+    int arr[len];
+    for(int i=0; i<len; i++){
+        printf("element - %d : ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    min = arr[0];
+    for(int i=1; i<len; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+
+    int secondSmallest = 9999;
+    for(int i=0; i<len; i++){
+        if(arr[i] == min){
+            continue;
+        }else{
+            if(secondSmallest > arr[i]){
+                secondSmallest = arr[i];
+            }
+        }
+    }
+
+    printf("The Second smallest element in the array is : %d\n", secondSmallest);
+}
 
 int main(){
     // answerNumber1();
@@ -733,7 +800,8 @@ int main(){
     // answerNumber13();
     // answerNumber14();
     // answerNumber15();
-    answerNumber16();
+    // answerNumber16();
+    // answerNumber17();
     return 0;
 }
 
