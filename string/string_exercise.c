06bbdbf43174d37f0dc7abd8eb7f0ea20f3f1cbb
fadlydestriana <97951046+fdldestriana@users.noreply.github.com>
2024-03-01 11:47:54 +0700

    solving 3rd problem from string exercise in C string_exercise.c

--- string/string_exercise.c
+++ string/string_exercise.c
@@ -30,6 +30,30 @@ void answerNumber1(){
      
 }
 
+/*
+3. Write a program in C to separate individual characters from a string.
+
+Test Data :
+Input the string : w3resource.com
+
+Expected Output :
+
+The characters of the string are : 
+w  3  r  e  s  o  u  r  c  e  .  c
+*/
+
+void answerNumber3(){
+    char str[100];
+    int index = 0;
+    printf("Input the string : ");
+    scanf("%s", str);
+    
+    while(str[index] != '\0'){
+        printf("%c ", str[index]);
+        index++;
+    }
+}
+
 int main(){
     answerNumber1();
     return 0;
