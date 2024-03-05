b6118864345fac8cdc3954b5a4576ee672e11427
fadlydestriana <fadlydestriana@gmail.com>
2024-03-05 14:29:31 +0700

    merged with online repository

--- string/string_exercise.c
+++ string/string_exercise.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-
 /*
-Write a function to rearrange the string
-char name = "fadly" -> "yldaf"
+0.  Write a function to reversed the string
+    char name = "fadly" -> "yldaf"
 */
 
-void answerNumber1(){
+void answerNumber0(){
     int len = 0, index = 0, subtractor = 1;
-    char name[] = "michael";
+    char name[] = "w3resource.com";
 
     while (name[index] != '\0')
     {
@@ -31,6 +30,7 @@ void answerNumber1(){
 }
 
 /*
+<<<<<<< Updated upstream
 3. Write a program in C to separate individual characters from a string.
 
 Test Data :
@@ -75,5 +75,55 @@ void answerNumber4(){
 
 int main(){
     answerNumber4();
+=======
+1.  Write a program in C to input a string and print it.
+    Test Data :
+    Input the string : Welcome, w3resource  
+    Expected Output :
+    The string you entered is : Welcome, w3resource 
+*/
+
+void answerNumber1(){
+    char str1[50]; 
+    char str2[50];
+
+    printf("Input the string : ");
+    scanf("%s", str1);
+    scanf("%s", str2);
+
+    printf("The string you entered is : %s %s\n", str1, str2);
+}
+
+/*
+2.  Write a program in C to find the length of a string without using library functions.
+    Test Data :
+    Input the string : w3resource.com
+    Expected Output :
+    Length of the string is : 15 
+*/
+
+void answerNumber2(){
+    char str[50];
+    int index = 0;
+    int count = 0;
+
+    printf("Input the string : ");
+    scanf("%s", str);
+
+    while (str[index] != '\0')
+    {
+        index++;
+        count++;
+    }
+
+    printf("Length of the string is : %d\n", count);
+    
+}
+
+int main(){
+    answerNumber0();
+    // answerNumber1();
+    // answerNumber2();
+>>>>>>> Stashed changes
     return 0;
 }
